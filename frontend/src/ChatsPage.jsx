db207ead884a745089551617983dad8614e0c0a4
Abelti <abeltibbeshana@gmail.com>
2023-06-09 14:13:21 +0300

    final

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,7 +1,16 @@
+import {MultiChatSocket, MultiChatWindow, useMultiChatLogic} from 'react-chat-engine-advanced';
+
 const ChatsPage = () => {
-    return <>
-        chats...
-    </>
+    const chatProps = useMultiChatLogic(
+        '79027d68-52f0-43db-98ca-bb6df6285b12',
+        props.user.username,
+        props.user.secret
+    );
+    return <div style={{height: '100vh'}}>
+            <MultiChatSocket{...chatProps} />
+            <MultiChatWindow {...chatProps} style={{height: '100%'}} />
+        </div>
 }
+
 
 export default ChatsPage;
